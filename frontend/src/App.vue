ff121288f5b54d44905fc9f960bfe898dd14eaef
Temmel Adam <adam.temmel@bolagsverket.se>
2022-08-15 13:27:22 +0200

    First commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+//import { RouterLink, RouterView } from 'vue-router'
+import HomeView from "./views/HomeView.vue";
+</script>
+
+<template>
+  <HomeView />
+
+  <!--
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <RouterView />
+  -->
+</template>
+
+<style scoped>
+</style>
---
+++ frontend/src/components/ChatBubble.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+defineProps<{
+  isUser: boolean;
+  contents: string;
+}>();
+</script>
+
+<template>
+  <div class="bubble-row">
+    <div v-if="isUser" class="bubble user">
+      {{contents}}
+    </div>
+    <div v-else class="bubble stranger">
+      {{contents}}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.bubble-row {
+  width: 100%;
+  display: inline-block;
+  padding: 4px;
+}
+
+.bubble {
+  padding: 8px 12px;
+  border-radius: 16px;
+  max-width: 60%;
+  font-size: 1.1em;
+}
+
+.user {
+  background: #77D;
+  float: right;
+}
+
+.stranger {
+  background: #555;
+  float: left;
+}
+</style>
