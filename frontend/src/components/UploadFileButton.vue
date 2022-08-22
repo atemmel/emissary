3033e1c521e02fb512f8755b974f99e321342d3d
Temmel Adam <adam.temmel@bolagsverket.se>
2022-08-22 12:01:27 +0200

    Got a file selection dialog going

--- frontend/src/components/UploadFileButton.vue
+++ frontend/src/components/UploadFileButton.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
 import PlusIcon from "./PlusIcon.vue";
+
+const fileChanged = () => {
+  const files = document.getElementById("file-input").files;
+  if(files.length != 1) {
+    return;
+  }
+  console.log(files);
+};
 </script>
 
 <template>
+  <label for="file-input">
   <div class="upload-button">
-    <PlusIcon></PlusIcon>
+    <PlusIcon @click="selectFile"/>
   </div>
+  </label>
+  <input type="file" id="file-input" @change="fileChanged">
 </template>
 
 <style scoped>
@@ -20,5 +31,10 @@ import PlusIcon from "./PlusIcon.vue";
 .upload-button:hover {
   background: #555;
   cursor: pointer;
+}
+
+#file-input {
+  /* better for accessibility than outright removing the element */
+  position: absolute; left: -99999rem
 }
 </style>
