49de2a92b5aea3a1d1fa7b2e3bae5f8fe69d328e
Temmel Adam <adam.temmel@bolagsverket.se>
2022-08-24 10:59:05 +0200

    Refactored backend code

--- backend/src/main/java/emissarybackend/ChatMessageController.java
+++ backend/src/main/java/emissarybackend/ChatMessageController.java
@@ -1,18 +1,9 @@
 package emissarybackend;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import javax.transaction.Transactional;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,61 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 @RequestMapping("/api")
 class ChatMessageController {
-	private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
 	private final ChatMessageRepository chatRepo;
 
-	private final ChatConversationRepository conversationRepo;
-
-	private final ChatMessageAttachmentRepository attachmentRepo;
-
-	ChatMessageController(ChatMessageRepository chatRepo, ChatConversationRepository conversationRepo, ChatMessageAttachmentRepository attachmentRepo) {
+	ChatMessageController(ChatMessageRepository chatRepo) {
 		this.chatRepo = chatRepo;
-		this.conversationRepo = conversationRepo;
-		this.attachmentRepo = attachmentRepo;
 	}
 
 	@GetMapping("/messages")
 	public List<ChatMessage> all() {
 		return chatRepo.findAll();
-	}
-
-	@MessageMapping("/chat/askhead/{id}")
-	@SendTo("/chat/head")
-	@Transactional
-	public Map<String, Object> head(@PathVariable("id") Long conversationId) {
-		var conversation = conversationRepo.findById(conversationId).orElseThrow(
-			() -> new RuntimeException("Could not find conversation with id " + conversationId));
-		final var head = conversation.getMessages().size();
-		return Map.of("head", head);
-	}
-
-	// client publishes to this
-	@MessageMapping("/chat/send")
-	// client subscribes to this
-	@SendTo("/chat")
-	@Transactional
-	public ChatMessage newMessage(ChatMessage message) {
-		log.info("Begin message handling...");
-		var contents = message.getContents();
-		var attachment = message.getAttachment();
-		boolean lacksMessage = contents.isEmpty() || contents.isBlank();
-		boolean lacksAttachment = attachment == null || attachment.getBytes().length <= 0;
-		if(lacksMessage && lacksAttachment) {
-			log.info("Did not store message, as both the attachment and the message contents were empty");
-			return null;
-		}
-
-		if(!lacksAttachment) {
-			log.info("Saved new message attachment");
-			message.setAttachment(attachmentRepo.save(attachment));
-		}
-
-		message = chatRepo.save(message);
-		var convId = message.getConversation().getId();
-		var conv = conversationRepo.findById(convId).orElseThrow(() -> new RuntimeException("Could not find message recipient"));
-		conv.addMessage(message);
-		log.info("Saved new message");
-		return message;
 	}
 }
---
+++ backend/src/main/java/emissarybackend/RealtimeController.java
@@ -0,0 +1,69 @@
+package emissarybackend;
+
+import java.util.Map;
+
+import javax.transaction.Transactional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+class RealtimeController {
+	private static final Logger log = LoggerFactory.getLogger(RealtimeController.class);
+
+	private final ChatMessageRepository chatRepo;
+	private final ChatConversationRepository conversationRepo;
+	private final ChatMessageAttachmentRepository attachmentRepo;
+
+	RealtimeController(ChatMessageRepository chatRepo, 
+			ChatConversationRepository conversationRepo,
+			ChatMessageAttachmentRepository attachmentRepo) {
+		this.chatRepo = chatRepo;
+		this.conversationRepo = conversationRepo;
+		this.attachmentRepo = attachmentRepo;
+	}
+
+	// client publishes to this
+	@MessageMapping("/chat/send")
+	// client subscribes to this
+	@SendTo("/chat")
+	@Transactional
+	public ChatMessage newMessage(ChatMessage message) {
+		log.info("Begin message handling...");
+		final var contents = message.getContents();
+		final var attachment = message.getAttachment();
+		boolean lacksMessage = contents.isEmpty() || contents.isBlank();
+		boolean lacksAttachment = attachment == null || attachment.getBytes().length <= 0;
+		if(lacksMessage && lacksAttachment) {
+			log.info("Did not store message, as both the attachment and the message contents were empty");
+			return null;
+		}
+
+		if(!lacksAttachment) {
+			log.info("Saved new message attachment");
+			message.setAttachment(attachmentRepo.save(attachment));
+		}
+
+		message = chatRepo.save(message);
+		final var convId = message.getConversation().getId();
+		final var conv = conversationRepo.findById(convId).orElseThrow(() -> new RuntimeException("Could not find message recipient"));
+		conv.addMessage(message);
+		log.info("Saved new message");
+		return message;
+	}
+
+	@MessageMapping("/chat/askhead/{id}")
+	@SendTo("/chat/head")
+	@Transactional
+	public Map<String, Object> head(@PathVariable("id") Long conversationId) {
+		final var conversation = conversationRepo.findById(conversationId).orElseThrow(
+			() -> new RuntimeException("Could not finc conversation with id " + conversationId));
+		final var head = conversation.getMessages().size();
+		return Map.of("head", head);
+	}
+
+}
