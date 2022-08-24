874f2ef4e2a9f7ff3b40a9576e1786bbd182bd15
Temmel Adam <adam.temmel@bolagsverket.se>
2022-08-24 10:18:25 +0200

    Can now download uploaded text files

--- frontend/src/components/ChatBubble.vue
+++ frontend/src/components/ChatBubble.vue
@@ -15,26 +15,86 @@ const isImage = () => attachment
     && attachment.bytes
     && attachment.type.startsWith("image/");
 
+const isPlaintext = () => attachment
+    && attachment.bytes;
+
+const isVideo = () => attachment
+    && attachment.bytes
+    && attachment.type.startsWith("video/");
+
 const src: string = attachment
     ? `data:${attachment?.type};base64,${attachment?.bytes}`
     : "";
+
+const transformClass = () => {
+  return {
+    'user': isUser(props.message.author),
+    'stranger': !isUser(props.message.author),
+  };
+}
+
+const transformBgClass = () => {
+  return {
+    'user user-bg': isUser(props.message.author),
+    'stranger stranger-bg': !isUser(props.message.author),
+  };
+};
+
+const fileSize = () => attachment
+    ? attachment.bytes.length * (3.0/4.0)
+    : 0;
+
+const fileStr = () => {
+    const size = fileSize();
+    const kb = 1024;
+    const mb = kb * kb;
+    if(size < kb) {
+      return size + " bytes";
+    } else if(size < mb) {
+      return size + " kb";
+    }
+    return size + " mb";
+};
+
+const download = () => {
+  if(attachment == null) {
+    return;
+  }
+  const a = document.createElement("a"); //Create <a>
+  a.href = `data:${attachment.type};base64,${attachment.bytes}`; //Image Base64 Goes here
+  a.download = attachment.name; //File name Here
+  a.click(); //Downloaded file
+};
 
 </script>
 
 <template>
   <div class="bubble-row">
     <div>
+      <!-- Regular message -->
       <div v-if="message.attachment == null" 
-        :class="{'user user-bg': isUser(message.author), 'stranger stranger-bg': !isUser(message.author)}"
+        :class="transformBgClass()"
         class="bubble">
         {{message.contents}}
       </div>
+      <!-- Image -->
       <img v-else-if="isImage()"
         class="img"
-        :class="{'user': isUser(message.author), 'stranger': !isUser(message.author)}"
+        :class="transformClass()"
         :src="src" 
         :alt="message.attachment ? message.attachment.name : 'null'"
         />
+      <!-- Other attachment -->
+      <div v-else :class="transformBgClass()" class="bubble">
+        <div class="file" @click="download">
+          <div>
+            {{message.attachment.name}}
+          </div>
+          <div>
+            📄 {{fileStr()}}
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -73,5 +133,9 @@ const src: string = attachment
 .img {
   border-radius: 16px;
   max-width: 100%;
+}
+
+.file:hover {
+  cursor: pointer;
 }
 </style>
